04e7f0204a6da584931cca901b3ff1927e9436ed
Tien Pham <tiendev94@gmail.com>
2019-07-17 22:15:48 +0300

    End

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,24 +1,38 @@
 import { loginUser } from "../lib/auth";
+import Router from "next/router";
 
 class LoginForm extends React.Component {
   state = {
-    email: "Sincere@april.biz",
-    password: "hildegard.org"
+    email: "Rey.Padberg@karina.biz",
+    password: "ambrose.net",
+    error: "",
+    isLoading: false
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = event => {
     const { email, password } = this.state;
 
-    e.preventDefault();
-    loginUser(email, password);
+    event.preventDefault();
+    this.setState({ error: "", isLoading: true });
+    loginUser(email, password)
+      .then(() => {
+        Router.push("/profile");
+      })
+      .catch(this.showError);
+  };
+
+  showError = err => {
+    console.error(err);
+    const error = (err.response && err.response.data) || err.message;
+    this.setState({ error, isLoading: false });
   };
 
   render() {
-    const {email, password} = this.state
+    const { email, password, error, isLoading } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
@@ -40,7 +54,10 @@ class LoginForm extends React.Component {
             onChange={this.handleChange}
           />
         </div>
-        <button type="submit">Submit</button>
+        <button disabled={isLoading} type="submit">
+          {isLoading ? "Sending" : "Submit"}
+        </button>
+        {error && <div>{error}</div>}
       </form>
     );
   }
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const AUTH_USER_TYPE = "authenticated";
-const COOKIE_SECRET = "fasdfasdfs";
+const COOKIE_SECRET = "asldkfjals23ljk";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: !dev,
@@ -20,7 +20,6 @@ const authenticate = async (email, password) => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/users"
   );
-
   return data.find(user => {
     if (user.email === email && user.website === password) {
       return user;
@@ -40,19 +39,33 @@ app.prepare().then(() => {
     if (!user) {
       return res.status(403).send("Invalid email or password");
     }
-
     const userData = {
       name: user.name,
       email: user.email,
       type: AUTH_USER_TYPE
     };
-
     res.cookie("token", userData, COOKIE_OPTIONS);
-
     res.json(userData);
   });
 
-  // Pass requests through so nextjs can handle
+  server.post("/api/logout", (req, res) => {
+    res.clearCookie("token", COOKIE_OPTIONS);
+    res.sendStatus(204);
+  });
+
+  server.get("/api/profile", async (req, res) => {
+    const { signedCookies = {} } = req;
+    const { token } = signedCookies;
+    if (token && token.email) {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const userProfile = data.find(user => user.email === token.email);
+      return res.json({ user: userProfile });
+    }
+    res.sendStatus(404);
+  });
+
   server.get("*", (req, res) => {
     return handle(req, res);
   });
